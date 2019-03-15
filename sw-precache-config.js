1ed4179c5e027b079dc899e4637fc89969b4846f
Duoc Nguyen <nguyenvanduocit@gmail.com>
2019-03-15 12:36:18 +0700

    support offline utils

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,8 +1,20 @@
 module.exports = {
-  root:'public',
+  root: 'public',
   staticFileGlobs: [
     'public/**/*.{html,jpg,png,css,js}',
   ],
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/cdn\.jsdelivr\.net\//,
+      handler: 'cacheFirst'
+    },
+    {
+      urlPattern: /^https:\/\/fonts\.googleapis\.com\//,
+      handler: 'cacheFirst'
+    },{
+      urlPattern: /^https:\/\/fonts\.gstatic\.com\//,
+      handler: 'cacheFirst'
+    }],
   navigateFallback: '/offline.html',
   stripPrefix: 'public'
-};
+}
